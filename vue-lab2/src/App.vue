cea85cfc6e019ef13b068e25cd0e7dd95111b898
Boki Chan <onigirisamasan@gmail.com>
2026-07-12 18:15:31 +0700

    Create a mock data from fake database and Add new folder for fatchdata from data base.

--- vue-lab2/src/App.vue
+++ vue-lab2/src/App.vue
@@ -7,12 +7,11 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> | 
+          <RouterLink to="/">Event</RouterLink> | 
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
-    <h1>Event For Good</h1>
     <RouterView />
   </div>
 </template>
---
+++ vue-lab2/src/views/EventListView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+  import EventCard from '@/components/EventCard.vue'
+  import EventMetadata from '@/components/EventMetadata.vue'
+  import type { Event } from '@/types'
+  import { ref, onMounted } from 'vue'
+  import EventService from '@/services/EventService'
+  const events = ref<Event[] | null>(null)
+
+  onMounted(() => {
+    EventService.getEvents()
+    .then((response) => {
+      console.log(response.data)
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+  })
+</script>
+
+<template>
+  <h1>Event For Good</h1>
+  <div class="events">
+    <div class="event-wrapper" v-for="event in events" :key="event.id">
+      <EventCard :event="event" />
+      <EventMetadata :event="event"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .event-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 20px;
+    margin-bottom: 10px;
+
+  }
+</style>
