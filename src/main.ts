53017907f2d7d2e1ad90737a5618cc4b6560506c
Boki Chan <onigirisamasan@gmail.com>
2026-08-02 10:59:19 +0700

    Add inject

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
 
-const app = createApp(App)    
+const app = createApp(App)   
+inject()
 app.use(createPinia())
 app.use(router)
 
