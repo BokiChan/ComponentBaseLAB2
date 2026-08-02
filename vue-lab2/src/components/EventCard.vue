091d43900821bd97c7e42f24a93d8a5a7021faff
Boki Chan <onigirisamasan@gmail.com>
2026-08-02 09:44:51 +0700

    Add the theme in style.css and Change EventCard
    Change EventCard's CSS to use Tailwind.

--- vue-lab2/src/components/EventCard.vue
+++ vue-lab2/src/components/EventCard.vue
@@ -22,7 +22,8 @@ defineProps<{
 
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
-    <div class="event-card">
+    <div class="cursor-pointer border border-gray-300 p-[20px] w-[250px] mb-[18px]
+    hover:scale-101 hover:shadow-sp">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
@@ -43,10 +44,10 @@ defineProps<{
   margin-bottom: 18px;
 }
 
-.event-card:hover {
+/* .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+} */
 
 .event-link {
   text-decoration: none;
